69d74aad6da522f68cc65179d0117f476f226f70
chck <iwazaki_yuki@cyberagent.co.jp>
2017-11-14 12:45:43 +0900

    Add github.js to screenshot progress page

---
+++ config.example.js
@@ -0,0 +1,13 @@
+module.exports = {
+  githubId: 'YOUR_GITHUB_ID',
+  githubPw: 'YOUR_GITHUB_PW',
+  githubProjectUrl: 'YOUR_GITHUB_PROJECT_URL',
+  viewportWidth: 2560,
+  viewportHeight: 1024,
+  clipX: 678,
+  clipY: 245,
+  clipWidth: 1000,
+  clipHeight: 450,
+  esaTeamId: 'YOUR_ESA_TEAM_ID',
+  esaAccessToken: 'YOUR_ESA_TOKEN',
+};
---
+++ github.js
@@ -0,0 +1,36 @@
+const puppeteer = require('puppeteer');
+const config = require('./config');
+
+async function login(page) {
+  await page.goto('https://github.com/login');
+  await page.waitForSelector('#login_field');
+  await page.type('#login_field', config.githubId);
+  await page.type('#password', config.githubPw);
+  const button = await page.$('input[type="submit"]');
+  await button.click();
+  await page.waitFor(3000);
+}
+
+async function fetchProgress() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({width: config.viewportWidth, height: config.viewportHeight});
+  await login(page);
+  await page.goto(config.githubProjectUrl);
+  await page.waitFor(3000);
+  await page.screenshot({
+    path: 'progress.png',
+    clip: {
+      'x': config.clipX,
+      'y': config.clipY,
+      'width': config.clipWidth,
+      'height': config.clipHeight,
+    },
+  });
+  await browser.close();
+}
+
+
+module.exports = {
+  fetchProgress,
+};
